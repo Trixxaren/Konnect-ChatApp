dd5a15f62c2d018e9e8a3794fd22d4f5fa183ecc
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-08-27 10:59:54 +0200

    GuestRoute fixed with and codcomments

--- src/routes/GuestRoute.jsx
+++ src/routes/GuestRoute.jsx
@@ -1,9 +1,16 @@
-// En väktare vid vakten. Visa armbandet för att komma i, har du redan ett armband (token) skickas in till festen (/chat)
-// Släpper bara in gäster (utan token). Har du token, skickas till /chat
+// En "väktare" vid dörren som kollar armband (token).
+// Om du redan har ett armband (token), skickas du direkt in i festen (/chat).
+// Om du INTE har armband, får du passera in till "gäst-området" (Outlet, t.ex. /login eller /register).
 import { Navigate, Outlet } from "react-router-dom";
+// Navigate skickar användaren till en annan route (redirect).
+// Outlet platsen där de barn-routes (t.ex. Login, Register) kommer att renderas.
 import { useAuth } from "../auth/AuthContext";
 
 export default function GuestRoute() {
   const { token } = useAuth();
+  // Hämtar token från AuthContext.
+  // kikar om den den här besökaren redan ett armband (token).
+
   return token ? <Navigate to="/chat" replace /> : <Outlet />;
+  // Om token finns så skicka till /chat om inte skickas till barn-routes (Outlet) /login eller /register.
 }
