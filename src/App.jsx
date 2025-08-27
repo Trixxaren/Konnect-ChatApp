40685a4401412542be8d718075b683853b8fa65f
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-08-27 11:29:17 +0200

    Favicon and CSP updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,28 @@ import GuestRoute from "./routes/GuestRoute.jsx";
 export default function App() {
   return (
     <Routes>
-      {/* Start på /login */}
+      {/* om någon går till Root "/" skickas till login*/}
       <Route path="/" element={<Navigate to="/login" replace />} />
 
-      {/* Gäst-område: bara om man INTE är inloggad */}
+      {/* "Gäst område" routes som bara får visas om man INTE är inloggad */}
+      {/* GuestRoute kollar om du redan har token för att sedan skicka till /chat */}
+      {/* Om du ej är inloggad får du se dessa "barn-routes" */}
       <Route element={<GuestRoute />}>
         {" "}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Route>
 
-      {/* Skyddat område: kräver token */}
+      {/* Skyddat område för routes som bara får visas om man är inloggad */}
+      {/* ProtectedRoute kollar om du har token för att sedan skickas till /chat */}
+      {/* Om inte har token redirect till /login */}
       <Route element={<ProtectedRoute />}>
         {" "}
         <Route path="/chat" element={<Chat />} />
       </Route>
 
-      {/* 404 fallback (valfritt) */}
+      {/* Fallback för att redirecta om man försöker gå till en "okänd" path, då skickas man tillbaka till /login* som en 404./}
+      {/* path="*" catch-all görs så att man skickas tillbaka till /login om man försöker med en okänd sida som t ex /abc, /hejsan..  */}
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,15 @@ import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
+// BrowserRouter som ger hela appen routing-funktionalitet.
+// Så vi kan använda <Routes>, <Route>, <Navigate> osv. i App.jsx)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* AuthProvider lägger AuthContext runt appen.
+          Det betyder att vi kan nå token, user, login, logout 
+          från vilken komponent som helst i App. */}
       <AuthProvider>
         <App />
       </AuthProvider>
